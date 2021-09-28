c920f02b4eb44a2c372f25c05a10ad1e783f705d
bookkuun <kuboyuya@outlook.jp>
2021-09-28 14:51:04 +0900

    update: サインドバー改行

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <!-- Page Content -->
         <main class="flex">
             <div class="w-1/5">
-                <a href="{{ route('projects.index') }}">プロジェクト一覧</a>
+                <a href="{{ route('projects.index') }}">プロジェクト一覧</a><br>
                 <a href="{{ route('projects.create') }}">プロジェクト作成</a>
             </div>
             <div class="w-4/5">
