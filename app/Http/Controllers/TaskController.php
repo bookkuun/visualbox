33e47833834646e600b03d5a89b8b93b54c4a80d
bookkuun <kuboyuya@outlook.jp>
2021-09-28 14:08:54 +0900

    add: タスク登録機能の実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Task;
 use App\Models\TaskCategory;
 use App\Models\TaskKind;
 use App\Models\TaskStatus;
@@ -41,9 +42,38 @@ class TaskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Project $project)
     {
-        //
+        $request->validate([
+            'task_kind_id' => 'required|integer',
+            'name' => 'required|string|max:255',
+            'detail' => 'nullable|string|max:1000',
+            'task_status_id' => 'required|integer',
+            'assigner_id' => 'nullable|integer',
+            'task_category_id' => 'nullable|integer',
+            'task_resolution_id' => 'nullable|integer',
+            'due_date' => 'nullable|date',
+        ]);
+
+        if (Task::create([
+            'project_id' => $project->id,
+            'task_kind_id' => $request->task_kind_id,
+            'name' => $request->name,
+            'detail' => $request->detail,
+            'task_status_id' => $request->task_status_id,
+            'assigner_id' => $request->assigner_id,
+            'task_category_id' => $request->task_category_id,
+            'due_date' => $request->due_date,
+            'created_user_id' => $request->user()->id,
+        ])) {
+            $flash = ['success' => __('Task created successfully.')];
+        } else {
+            $flash = ['error' => __('Failed to create the task.')];
+        }
+
+        return redirect()
+            ->route('tasks.index', compact('project'))
+            ->with($flash);
     }
 
     /**
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * マスアサインメントが可能な属性
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'project_id',
+        'name',
+        'task_kind_id',
+        'detail',
+        'task_status_id',
+        'created_user_id',
+        'updated_user_id',
+        'assigner_id',
+        'task_category_id',
+        'due_date',
+        'task_resolution_id',
+    ];
 }
